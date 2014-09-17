42e8eff3ab10e504ee03c943bf00722d7ca04325
Mickael van der Beek <mickael@sagacify.com>
2014-09-17 15:06:49 +0200

    Added tests that cover close to 100% of the code base. One branch can't be triggered and the other depends on a Node.js bug.

--- src/hashing/hash.js
+++ src/hashing/hash.js
@@ -10,6 +10,22 @@ module.exports = (function () {
 
 	}
 
+	Hash.prototype.hashToken = function (token, callback) {
+		var hashedToken;
+
+		try {
+			hashedToken = crypto
+			.createHash('sha256')
+			.update(token)
+			.digest('hex');
+		}
+		catch(e) {
+			return callback(e);
+		}
+
+		return callback(null, hashedToken);
+	};
+
 	Hash.prototype.hashPassword = function (password, callback) {
 		bcrypt.genSalt(function (e, salt) {
 			if (e) {
@@ -20,29 +36,33 @@ module.exports = (function () {
 		});
 	};
 
-	Hash.prototype.compareToken = function (token, hash) {
-		return this.hashToken(token) === hash;
+	Hash.prototype.compareToken = function (token, hash, callback) {
+		this.hashToken(token, function (e, hashedToken) {
+			if(e) {
+				return callback(e);
+			}
+
+			callback(null, hashedToken === hash);
+		});
 	};
 
 	Hash.prototype.comparePassword = function (password, hash, callback) {
 		bcrypt.compare(password, hash, callback);
 	};
 
-	Hash.prototype.genereateToken = function (callback) {
-		crypto.randomBytes(128, function (e, buffer) {
+	Hash.prototype.genereateToken = function (size, callback) {
+		if(callback === undefined) {
+			callback = size;
+			size = 128;
+		}
+
+		crypto.randomBytes(size, function (e, buffer) {
 			if (e) {
 				return callback(e);
 			}
 
 			callback(e, buffer.toString('hex'));
 		});
-	};
-
-	Hash.prototype.hashToken = function (token) {
-		return crypto
-		.createHash('sha256')
-		.update(token)
-		.digest('hex');
 	};
 
 	return Hash;
---
+++ test/test.js
@@ -0,0 +1,350 @@
+var SGSCrypto = require('./coverage/instrument/src/sgs-crypto');
+var SGSHash = new (SGSCrypto.Hash)();
+
+var chiSquare = require('chi-square');
+var assert = require('assert');
+var async = require('async');
+
+describe('Testing the Crypto module:', function () {
+	'use strict';
+
+	describe('BCrypt password hashes:', function () {
+
+		it('Password is hashed', function (callback) {
+			var password = 'test';
+			SGSHash.hashPassword(password, function (e, hash) {
+				if (e) {
+					return callback(e);
+				}
+
+				assert.deepEqual(typeof hash, 'string');
+				assert.notDeepEqual(hash, password);
+				callback();
+			});
+		});
+
+		it('Password hashing handles errors', function (callback) {
+			SGSHash.hashPassword(undefined, function (e, hash) {
+				assert.deepEqual(e instanceof Error, true);
+				assert.deepEqual(hash, undefined);
+				callback();
+			});
+		});
+
+		it('Password hash is valid Bcrypt hash', function (callback) {
+			var password = 'test';
+			SGSHash.hashPassword(password, function (e, hash) {
+				if (e) {
+					return callback(e);
+				}
+
+				assert.deepEqual(typeof hash, 'string');
+				assert.deepEqual(hash.length, 60);
+
+				var components = hash.split('$');
+				components.shift();
+
+				assert.deepEqual(components.length, 3);
+
+				var version = components[0];
+				var rounds = components[1];
+				var encrypted = components[2];
+
+				assert.deepEqual(version, '2a');
+				assert.deepEqual(+(rounds) > 5, true);
+				assert.deepEqual(+(rounds) < 13, true);
+
+				assert.deepEqual(typeof encrypted, 'string');
+				assert.deepEqual(encrypted.length, 53);
+				assert.notEqual(encrypted.match(/^[a-zA-Z0-9\.\/]{53}$/), null);
+				assert.deepEqual(encrypted.match(/^[a-zA-Z0-9\.\/]{53}$/)[0], encrypted);
+				callback();
+			});
+		});
+
+		it('Password has different hash every time', function (callback) {
+			var password = 'test';
+			async.parallel({
+				hashA: function (cb) {
+					SGSHash.hashPassword(password, cb);
+				},
+				hashB: function (cb) {
+					SGSHash.hashPassword(password, cb);
+				}
+			}, function (e, results) {
+				if (e) {
+					return callback(e);
+				}
+
+				var hashA = results.hashA;
+				var hashB = results.hashB;
+
+				assert.deepEqual(hashA.length, hashB.length);
+				assert.notEqual(hashA, hashB);
+				callback();
+			});
+		});
+
+		it('Password hash is not a hash of undefined, empty string or null', function (callback) {
+			var password = 'test';
+			SGSHash.hashPassword(password, function (e, hash) {
+				if (e) {
+					return callback(e);
+				}
+
+				async.parallel({
+					compareUndefined: function (cb) {
+						SGSHash.comparePassword('undefined', hash, cb);
+					},
+					compareNull: function (cb) {
+						SGSHash.comparePassword('null', hash, cb);
+					},
+					compareEmptyString: function (cb) {
+						SGSHash.comparePassword('', hash, cb);
+					}
+				}, function (e, results) {
+					if (e) {
+						return callback(e);
+					}
+
+					var isUndefinedHash = results.compareUndefined;
+					var isNullHash = results.compareNull;
+					var isEmptyStringHash = results.compareEmptyString;
+
+					assert.equal(isUndefinedHash, false);
+					assert.equal(isNullHash, false);
+					assert.equal(isEmptyStringHash, false);
+					callback();
+				});
+			});
+		});
+
+		it('Password can be hashed and compared with success', function (callback) {
+			var password = 'test';
+			async.waterfall([
+				function (cb) {
+					SGSHash.hashPassword(password, cb);
+				},
+				function (hash, cb) {
+					SGSHash.comparePassword(password, hash, cb);
+				}
+			], function (e, matches) {
+				if (e) {
+					return callback(e);
+				}
+
+				assert.deepEqual(matches, true);
+				callback();
+			});
+		});
+
+		it('Produced BCrypt hashes have a cryptographically strong randomness', function (callback) {
+			this.timeout(10 * 1000);
+			var password = 'test';
+			var rounds = 100;
+			async.times(rounds, function (i, cb) {
+				SGSHash.hashPassword(password, function (e, hash) {
+					if (e) {
+						return cb(e);
+					}
+
+					var distribution = chiSquare.calculate(new Buffer(hash, 'base64'));
+					cb(null, distribution);
+				});
+			}, function (e, distributions) {
+				if (e) {
+					return callback(e);
+				}
+
+				var sum = distributions.reduce(function (sum, distribution) {
+					return sum + distribution;
+				}, 0);
+				var avg = sum / rounds;
+
+				assert.deepEqual(avg < 0.6, true);
+				assert.deepEqual(avg > 0.4, true);
+				callback();
+			});
+		});
+
+	});
+
+	describe('Node.js\'s Crypto core module tokens' , function () {
+
+		it('Token is hashed', function (callback) {
+			var token;
+			async.waterfall([
+				function (cb) {
+					SGSHash.genereateToken(cb);
+				},
+				function (resToken, cb) {
+					token = resToken;
+					SGSHash.hashToken(resToken, cb);
+				}
+			], function (e, hashedToken) {
+				if (e) {
+					return callback(e);
+				}
+
+				assert.deepEqual(typeof hashedToken, 'string');
+				assert.notDeepEqual(hashedToken, token);
+				callback();
+			});
+		});
+
+		it('Token hashing handles errors', function (callback) {
+			SGSHash.hashToken(undefined, function (e, hashedToken) {
+				assert.deepEqual(e instanceof Error, true);
+				assert.deepEqual(hashedToken, undefined);
+				callback();
+			});
+		});
+
+		it('Token comparison handles errors', function (callback) {
+			async.waterfall([
+				function (cb) {
+					SGSHash.genereateToken(cb);
+				},
+				function (resToken, cb) {
+					SGSHash.hashToken(resToken, cb);
+				},
+			], function (e, hashedToken) {
+				if (e) {
+					return callback(e);
+				}
+
+				SGSHash.compareToken(undefined, hashedToken, function (e, matches) {
+					assert.deepEqual(e instanceof Error, true);
+					assert.deepEqual(matches, undefined);
+					callback();
+				});
+			});
+		});
+
+		// Broken until https://github.com/joyent/node/issues/5801 is merged
+		// it('Token generation handles errors', function (callback) {
+		// 	SGSHash.genereateToken(undefined, function (e, hashedToken) {
+		// 		assert.deepEqual(e instanceof Error, true);
+		// 		assert.deepEqual(hashedToken, undefined);
+		// 		callback();
+		// 	});
+		// });
+
+		it('Token hash is valid SHA256 hash', function (callback) {
+			async.waterfall([
+				function (cb) {
+					SGSHash.genereateToken(cb);
+				},
+				function (token, cb) {
+					SGSHash.hashToken(token, cb);
+				}
+			], function (e, hashedToken) {
+				if (e) {
+					return callback(e);
+				}
+
+				assert.deepEqual(typeof hashedToken, 'string');
+				assert.deepEqual(hashedToken.length, 64);
+				assert.notDeepEqual(hashedToken.match(/^[a-f0-9]{64}$/), null);
+				assert.deepEqual(hashedToken.match(/^[a-f0-9]{64}$/)[0], hashedToken);
+				callback();
+			});
+		});
+
+		it('Token hash is not a hash of undefined, empty string or null', function (callback) {
+			async.waterfall([
+				function (cb) {
+					SGSHash.genereateToken(cb);
+				},
+				function (token, cb) {
+					SGSHash.hashToken(token, cb);
+				}
+			], function (e, hashedToken) {
+				if (e) {
+					return callback(e);
+				}
+
+				async.parallel({
+					compareUndefined: function (cb) {
+						SGSHash.compareToken('undefined', hashedToken, cb);
+					},
+					compareNull: function (cb) {
+						SGSHash.compareToken('null', hashedToken, cb);
+					},
+					compareEmptyString: function(cb) {
+						SGSHash.compareToken('', hashedToken, cb);
+					}
+				}, function (e, results) {
+					if (e) {
+						return callback(e);
+					}
+
+					var isUndefinedHash = results.compareUndefined;
+					var isNullHash = results.compareNull;
+					var isEmptyStringHash = results.compareEmptyString;
+
+					assert.deepEqual(isUndefinedHash, false);
+					assert.deepEqual(isNullHash, false);
+					assert.deepEqual(isEmptyStringHash, false);
+					callback();
+				});
+			});
+		});
+
+		it('Token can be hashed and compared with success', function (callback) {
+			var token;
+			async.waterfall([
+				function (cb) {
+					SGSHash.genereateToken(cb);
+				},
+				function (resToken, cb) {
+					token = resToken;
+					SGSHash.hashToken(resToken, cb);
+				},
+			], function (e, hashedToken) {
+				if (e) {
+					return callback(e);
+				}
+
+				SGSHash.compareToken(token, hashedToken, function (e, matches) {
+					if (e) {
+						return callback(e);
+					}
+
+					assert.deepEqual(matches, true);
+					callback();
+				});
+			});
+		});
+
+		it('Produced SHA256 tokens have a cryptographically strong randomness', function (callback) {
+			this.timeout(10 * 1000);
+			var rounds = 100;
+			async.times(rounds, function (i, cb) {
+				SGSHash.genereateToken(function (e, token) {
+					if (e) {
+						return cb(e);
+					}
+
+					var distribution = chiSquare.calculate(new Buffer(token, 'hex'));
+					cb(null, distribution);
+				});
+			}, function (e, distributions) {
+				if (e) {
+					return callback(e);
+				}
+
+				var sum = distributions.reduce(function (sum, distribution) {
+					return sum + distribution;
+				}, 0);
+				var avg = sum / rounds;
+
+				assert.deepEqual(avg < 0.6, true);
+				assert.deepEqual(avg > 0.4, true);
+				callback();
+			});
+		});
+
+	});
+
+});
